397a7f5c622e574ab408b68631cea55b9ccd8bcf
lvcheng.journeyc <lvcheng.journeyc@bytedance.com>
2021-11-17 22:44:46 +0800

    Wed Nov 17 22:44:46 CST 2021

--- nvim/init.vim
+++
@@ -1,8 +0,0 @@
-" 加载base
-exec 'source' '~/.config/nvim/config/base.vim'
-
-" 加载插件
-exec 'source' '~/.config/nvim/config/plug.vim'
-
-" 加载主题
-exec 'source' '~/.config/nvim/config/theme.vim'
---
+++ nvim/lua/core/init.lua
@@ -0,0 +1,16 @@
+local fn = vim.fn
+
+function basic_configuration()
+
+end
+
+function plugins_management()
+
+end
+
+function core()
+	basic_configuration()
+	plugins_management()
+end
+
+core()
